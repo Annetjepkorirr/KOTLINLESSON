45e86fb3726a2b630e4e070a98ee02076d5e5371
Annetjepkorirr <124133670+Annetjepkorirr@users.noreply.github.com>
2023-05-10 00:32:51 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,20 @@ fun main() {
     var numbers = number(arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62))
     println(numbers)
 
-    var person =Person("Annette",20,"5ft",40.0),
-    Person("Jane",21,"6ft",30.0)
+    var person = listOf( Person("Annette",20,"5ft",40.0),
+        Person("Jane",30,"4ft",50.0),
+        Person("Mary",21,"7ft",39.9)
+    )
+    println(person.sortedByDescending { it.age })
+
 
     var identifyNames = getNames(arrayOf("Mary","Jane","TIna"))
-    identifyNames.sortedArrayDescending()
+    println( identifyNames)
+
+
+    var cars = listOf(Car("kfc128k",100),
+        Car("kfr674y",200))
+    println(getMileage(cars))
 
 
 
@@ -43,4 +52,11 @@ fun getNames(names:Array<String>):Array<String>{
 
 //Write a function that takes in a list of Car objects each with a registration and mileage
 //attribute and returns the average mileage of all the vehicles in the list
-//data class ()
+data class Car (var regisration:String, var mileage:Int)
+fun getMileage(vehicles:List<Car>):Double{
+    var sum = 0
+    for (vehicle in vehicles){
+        sum+= vehicle.mileage
+    }
+    return sum.toDouble()/vehicles.size
+}
